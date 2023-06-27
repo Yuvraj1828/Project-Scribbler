c9309b92bff08bd9213b4cd420693015695ee1f0
Yuvraj1828 <117924804+Yuvraj1828@users.noreply.github.com>
2023-06-27 15:25:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+function showPopup() {
+    document.querySelector('.form').classList.add('showform');
+    document.querySelector('.backgroundImage').classList.add('showbackgroundImage');
+    document.querySelector('.Aposts').classList.add('showAposts');
+    document.querySelector('.Cposts').classList.add('showCposts');
+  
+}
+
+function showSigninForm() {
+    document.querySelector('.SignInForm').classList.add('showSignInForm');
+    document.querySelector('.backgroundImage').classList.add('showbackgroundImage');
+    document.querySelector('.Aposts').classList.add('showAposts');
+    document.querySelector('.Cposts').classList.add('showCposts');
+   
+}
+
+function showcpForm(){
+document.querySelector('.CreatePostForm').classList.add('showCreatePostForm');
+document.querySelector('.backgroundImage').classList.add('showbackgroundImage');
+    document.querySelector('.Aposts').classList.add('showAposts');
+    document.querySelector('.Cposts').classList.add('showCposts');
+}
+
+
+function closeform(){
+    form.style.display='none';
+}
+
+  
---
+++ postlist.js
@@ -0,0 +1,15 @@
+function showPopup() {
+    document.querySelector('.form').classList.add('showform');
+    document.querySelector('.backgroundImage').classList.add('showbackgroundImage');
+    document.querySelector('.Aposts').classList.add('showAposts');
+    document.querySelector('.Cposts').classList.add('showCposts');
+  
+}
+
+function showSigninForm() {
+    document.querySelector('.SignInForm').classList.add('showSignInForm');
+    document.querySelector('.backgroundImage').classList.add('showbackgroundImage');
+    document.querySelector('.Aposts').classList.add('showAposts');
+    document.querySelector('.Cposts').classList.add('showCposts');
+   
+}
